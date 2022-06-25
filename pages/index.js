a7525471993ec387451497f9337ee0ed5f97014d
Subhan Ul Haque <iamhaque@gmail.com>
2022-06-25 14:25:58 +0530

    Added high score function

--- pages/index.js
+++ pages/index.js
@@ -1,160 +1,31 @@
 import { useEffect, useState } from "react";
 
+import { mapRange, initializeTiles } from "../shared/utils";
+
 import Head from "next/head";
 import Tile from "../components/tile";
 import GridSelectButton from "../components/grid-selection-button";
 
-const allEmojis = [
-  "✌",
-  "😂",
-  "😝",
-  "😁",
-  "😱",
-  "👉",
-  "🙌",
-  "🍻",
-  "🔥",
-  "🌈",
-  "☀",
-  "🎈",
-  "🌹",
-  "💄",
-  "🎀",
-  "⚽",
-  "🎾",
-  "🏁",
-  "😡",
-  "👿",
-  "🐻",
-  "🐶",
-  "🐬",
-  "🐟",
-  "🍀",
-  "👀",
-  "🚗",
-  "🍎",
-  "💝",
-  "💙",
-  "👌",
-  "❤",
-  "😍",
-  "😉",
-  "😓",
-  "😳",
-  "💪",
-  "💩",
-  "🍸",
-  "🔑",
-  "💖",
-  "🌟",
-  "🎉",
-  "🌺",
-  "🎶",
-  "👠",
-  "🏈",
-  "⚾",
-  "🏆",
-  "👽",
-  "💀",
-  "🐵",
-  "🐮",
-  "🐩",
-  "🐎",
-  "💣",
-  "👃",
-  "👂",
-  "🍓",
-  "💘",
-  "💜",
-  "👊",
-  "💋",
-  "😘",
-  "😜",
-  "😵",
-  "🙏",
-  "👋",
-  "🚽",
-  "💃",
-  "💎",
-  "🚀",
-  "🌙",
-  "🎁",
-  "⛄",
-  "🌊",
-  "⛵",
-  "🏀",
-  "🎱",
-  "💰",
-  "👶",
-  "👸",
-  "🐰",
-  "🐷",
-  "🐍",
-  "🐫",
-  "🔫",
-  "👄",
-  "🚲",
-  "🍉",
-  "💛",
-  "💚",
-];
-
-const generateTiles = (content, count) => {
-  const generatedTiles = [];
-  for (let i = 0; i < count * count; i++) {
-    const tile = {
-      id: i,
-      matched: false,
-      flipped: false,
-      content: content[i],
-    };
-
-    generatedTiles.push(tile);
-  }
-  return generatedTiles;
-};
-
-const generateEmojis = (count) => {
-  let emojis = [];
-  let emojisGenerated = 0;
-
-  while (emojisGenerated < count) {
-    let randomEmoji = allEmojis[Math.floor(Math.random() * allEmojis.length)];
-    if (emojis.includes(randomEmoji)) continue;
-
-    emojis.push(randomEmoji);
-    emojis.push(randomEmoji);
-    emojisGenerated += 1;
-  }
-
-  return shuffleArray(emojis);
-};
-
-const shuffleArray = (list) => {
-  for (var index = list.length - 1; index > 0; index--) {
-    const randomIndex = Math.floor(Math.random() * (index + 1));
-
-    const tempValue = list[index];
-    list[index] = list[randomIndex];
-    list[randomIndex] = tempValue;
-  }
-  return list;
-};
-
-const mapRange = (value, low1, high1, low2, high2) =>
-  low2 + ((high2 - low2) * (value - low1)) / (high1 - low1);
-
 export default function Home() {
-  const gridSizes = [2, 4, 6, 8];
+  const GRID_SIZES = [2, 4, 6, 8];
 
   const [gameState, setGameState] = useState({
     tiles: [],
     newGame: true,
+    gameWon: false,
     gameOver: false,
     gridSize: undefined,
-    totalAttempts: undefined,
+    highScores: undefined,
     uniqueTiles: undefined,
+    totalAttempts: undefined,
     remainingAttempts: undefined,
+  });
+
+  const [userDeviceData, setUserDeviceData] = useState({
+    langStatus: null,
+    userVendor: null,
+    userAgentStatus: null,
+    userAgentDataStatus: null,
   });
 
   const [timer, setTimer] = useState(0);
@@ -162,15 +33,40 @@ export default function Home() {
 
   const [prevFlippedTileIndex, setPrevFlippedTileIndex] = useState(undefined);
 
+  // Initialize game data on page load
   useEffect(() => {
-    initializeGame();
+    restartGame();
+
+    setUserDeviceData({
+      langStatus: navigator.language,
+      userVendor: navigator.vendor,
+      userAgentStatus: navigator.userAgent,
+      userAgentDataStatus: navigator.userAgentData,
+    });
   }, []);
 
+  // Update game state
   useEffect(() => {
     if (gameState.gameOver) {
+      // Save high score to local storage
+      const currentScore = Math.floor(
+        (gameState.remainingAttempts / gameState.totalAttempts) * 100
+      );
+      if (currentScore > gameState.highScores[gameState.gridSize]) {
+        const updatedHighScores = { ...gameState.highScores };
+        updatedHighScores[gameState.gridSize] = currentScore;
+
+        localStorage.setItem(
+          "tileMatchHighScores",
+          JSON.stringify(updatedHighScores)
+        );
+        setGameState({ ...gameState, highScores: updatedHighScores });
+      }
+
       resetTimer();
       return;
     }
+
     if (gameState.tiles.length <= 0) return;
 
     if (gameState.remainingAttempts <= 0) {
@@ -184,9 +80,20 @@ export default function Home() {
       hasWon = false;
       break;
     }
-    if (hasWon) setGameState({ ...gameState, gameOver: true });
+    if (hasWon) setGameState({ ...gameState, gameOver: true, gameWon: true });
   }, [gameState]);
 
+  // Decrement remaining attempts every 5 seconds of inactivity
+  useEffect(() => {
+    if (timer % 5 === 0 && timer > 0) {
+      setGameState({
+        ...gameState,
+        remainingAttempts: gameState.remainingAttempts - 1,
+      });
+    }
+  }, [timer]);
+
+  // Update timer every second
   useEffect(() => {
     let interval = null;
     if (isTimerActive) {
@@ -200,14 +107,43 @@ export default function Home() {
     return () => clearInterval(interval);
   }, [timer, isTimerActive]);
 
-  useEffect(() => {
-    if (timer % 5 === 0 && timer > 0) {
-      setGameState({
-        ...gameState,
-        remainingAttempts: gameState.remainingAttempts - 1,
-      });
+  // Initializes the game state
+  const initializeGame = (gridSize = 2, isNewGame = true) => {
+    const uniqueTiles = (gridSize * gridSize) / 2;
+    const generatedTiles = initializeTiles(uniqueTiles, gridSize);
+
+    let localHighScores = JSON.parse(
+      localStorage.getItem("tileMatchHighScores")
+    );
+    if (!localHighScores) {
+      localHighScores = {
+        2: 0,
+        4: 0,
+        6: 0,
+        8: 0,
+      };
     }
-  }, [timer]);
+
+    const newGameState = {
+      gameWon: false,
+      gameOver: false,
+      gridSize: gridSize,
+      newGame: isNewGame,
+      tiles: generatedTiles,
+      uniqueTiles: uniqueTiles,
+      highScores: localHighScores,
+      totalAttempts: Math.floor(uniqueTiles * 2.25),
+      remainingAttempts: Math.floor(uniqueTiles * 2.25),
+    };
+
+    setGameState(newGameState);
+    setPrevFlippedTileIndex(-1);
+  };
+
+  // Reset game state
+  const restartGame = () => {
+    initializeGame();
+  };
 
   const toggleTimer = () => {
     setIsTimerActive(!isTimerActive);
@@ -222,31 +158,11 @@ export default function Home() {
     setIsTimerActive(false);
   };
 
-  const initializeGame = (gridSize = 2, isNewGame = true) => {
-    const uniqueTiles = (gridSize * gridSize) / 2;
-    const generatedTiles = initializeTiles(uniqueTiles, gridSize);
-
-    const newGameState = {
-      gameOver: false,
-      newGame: isNewGame,
-      gridSize: gridSize,
-      tiles: generatedTiles,
-      uniqueTiles: uniqueTiles,
-      totalAttempts: Math.floor(uniqueTiles * 2.25),
-      remainingAttempts: Math.floor(uniqueTiles * 2.25),
-    };
-
-    setGameState(newGameState);
-    setPrevFlippedTileIndex(-1);
-  };
-
-  const initializeTiles = (emojiCount, tileCount) => {
-    const emojis = generateEmojis(emojiCount, tileCount);
-    return generateTiles(emojis, tileCount);
-  };
-
   const tileClickHandler = (tileID) => {
+    // Return if clicked on already matched tile
     if (gameState.tiles[tileID].matched) return;
+
+    // Return if clicked on last flipped tile
     if (prevFlippedTileIndex >= 0 && prevFlippedTileIndex === tileID) return;
 
     let isGameOver = false;
@@ -255,25 +171,29 @@ export default function Home() {
     if (prevFlippedTileIndex >= 0) {
       setPrevFlippedTileIndex(-1);
 
+      // Checks if last flipped tile and current tile match
       if (
-        prevFlippedTileIndex !== tileID &&
         gameState.tiles[prevFlippedTileIndex].content ===
-          gameState.tiles[tileID].content
+        gameState.tiles[tileID].content
       ) {
         gameState.tiles[tileID].matched = true;
         gameState.tiles[prevFlippedTileIndex].matched = true;
       } else {
-        // Consume attempt if guessed incorrectly
+        // Decrement attempt if guessed incorrectly
         remainingAttempts -= 1;
+
+        // Game is over if all attempts are used
         if (remainingAttempts <= 0) {
           isGameOver = true;
         }
+
         restartTimer();
       }
     } else {
       setPrevFlippedTileIndex(tileID);
     }
 
+    // Flip tiles
     const flippedTiles = gameState.tiles.map((tile) => {
       return {
         ...tile,
@@ -294,105 +214,109 @@ export default function Home() {
     });
   };
 
-  const buttonClickHandler = (selectedGridSize) => {
+  const gridSelectionButtonClickHandler = (selectedGridSize) => {
     initializeGame(selectedGridSize, false);
     toggleTimer();
   };
 
-  const restartGame = () => {
-    initializeGame();
-  };
+  // Show game-over overlay if game is over
+  let GameEndOverlay = <></>;
+  if (gameState.gameWon || gameState.gameOver) {
+    GameEndOverlay = (
+      <div className="gameOverScreen">
+        <header>
+          <p>{gameState.gameWon ? "You Won!!" : "You lose!!"}</p>
+        </header>
 
-  return (
-    <div className="container">
-      <Head>
-        <title>Tile Match</title>
-        <meta
-          name="description"
-          content="Flip memory game to match similar tiles"
-        />
-      </Head>
-
-      {gameState.gameOver ? (
-        <div className="gameOverScreen">
-          <header>
-            <p>Game Over</p>
-          </header>
-
-          <main>
-            <p>
-              Score: {gameState.remainingAttempts} / {gameState.totalAttempts}
-            </p>
-          </main>
-
-          <footer>
-            <div
-              className="button"
-              onClick={() => {
-                restartGame();
-              }}
-            >
-              <span>🔄</span>
-              <span>Replay</span>
-            </div>
-          </footer>
-        </div>
-      ) : (
-        <></>
-      )}
-
-      <div className="header">
-        <p className="title">Tile Match</p>
-        {gameState.newGame ? (
-          <p className="description">
-            Select a grid layout from the below options.
-            <br />
-            Each option scales exponentially in difficulty.
+        <main>
+          <p className="score">
+            <span>Score</span>
+            <span>
+              {Math.floor(
+                (gameState.remainingAttempts / gameState.totalAttempts) * 100
+              )}
+              %
+            </span>
           </p>
-        ) : (
-          <>
-            <p className="description">
-              You have{" "}
-              <span className="strong">{gameState.totalAttempts} attempts</span>{" "}
-              to match all the unmatched tiles. An attempt will be removed every{" "}
-              <span className="strong">5 seconds</span> unless guessed
-              incorrectly.
-            </p>
+          <p className="score highScore">
+            <span>High Score</span>
+            <span>{gameState.highScores[gameState.gridSize]}%</span>
+          </p>
+        </main>
 
-            <div className="progressContainer">
-              <div className="progressContainerTop">
-                <p className="progressTitle">Attempts remaining</p>
-                <p className="progressStatus">
-                  {gameState.remainingAttempts} / {gameState.totalAttempts}
-                </p>
-              </div>
-
-              <div
-                className="progressContainerBottom"
-                style={{
-                  "--progressBarRight": `${
-                    (gameState.remainingAttempts / gameState.totalAttempts) *
-                    100
-                  }%`,
-                }}
-              ></div>
-            </div>
-          </>
-        )}
+        <footer>
+          <div
+            className="restartGameButton"
+            onClick={() => {
+              restartGame();
+            }}
+          >
+            <span>Restart</span>
+          </div>
+        </footer>
       </div>
+    );
+  }
 
-      {gameState.newGame ? (
-        <div className="gridSelection main">
-          {gridSizes.map((item, index) => (
-            <GridSelectButton
-              hue={mapRange(index, 0, 3, 260, 360) * -1}
-              key={index}
-              gridSize={item}
-              clickHandler={buttonClickHandler}
-            />
-          ))}
+  // Set game header based on new game or running game
+  let GameHeaderContent;
+  if (gameState.newGame) {
+    GameHeaderContent = (
+      <p className="description">
+        Select a grid layout from the below options.
+        <br />
+        Each option scales exponentially in difficulty.
+      </p>
+    );
+  } else {
+    GameHeaderContent = (
+      <>
+        <p className="description">
+          You have{" "}
+          <span className="strong">{gameState.totalAttempts} attempts</span> to
+          match all the unmatched tiles. An attempt will be removed every{" "}
+          <span className="strong">5 seconds</span>.
+        </p>
+
+        <div className="progressContainer">
+          <div className="progressContainerTop">
+            <p className="progressTitle">Attempts remaining</p>
+            <p className="progressStatus">
+              {gameState.remainingAttempts} / {gameState.totalAttempts}
+            </p>
+          </div>
+
+          <div
+            className="progressContainerBottom"
+            style={{
+              "--progressBarRight": `${
+                (gameState.remainingAttempts / gameState.totalAttempts) * 100
+              }%`,
+            }}
+          ></div>
         </div>
-      ) : (
+      </>
+    );
+  }
+
+  // Set game main content based on new game or running game
+  let GameMainContent;
+  if (gameState.newGame) {
+    GameMainContent = (
+      <div className="gridSelection main">
+        {GRID_SIZES.map((item, index) => (
+          <GridSelectButton
+            key={index}
+            gridSize={item}
+            hue={mapRange(index, 0, 3, 260, 360) * -1}
+            clickHandler={gridSelectionButtonClickHandler}
+          />
+        ))}
+      </div>
+    );
+  } else {
+    GameMainContent = (
+      <>
         <div
           className="main"
           style={{
@@ -412,20 +336,38 @@ export default function Home() {
             />
           ))}
         </div>
-      )}
-
-      {!gameState.newGame ? (
         <div
-          className="restart"
+          className="restartMainButton"
           onClick={() => {
             restartGame();
           }}
         >
           Restart Game
         </div>
-      ) : (
-        <></>
-      )}
-    </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Tile Match</title>
+        <meta
+          name="description"
+          content="Flip memory game to match similar tiles"
+        />
+      </Head>
+
+      <div className="container">
+        {GameEndOverlay}
+
+        <div className="header">
+          <p className="title">Tile Match</p>
+          {GameHeaderContent}
+        </div>
+
+        {GameMainContent}
+      </div>
+    </>
   );
 }
---
+++ shared/utils.js
@@ -0,0 +1,153 @@
+const EMOJI_LIST = [
+  "✌",
+  "😂",
+  "😝",
+  "😁",
+  "😱",
+  "👉",
+  "🙌",
+  "🍻",
+  "🔥",
+  "🌈",
+  "☀",
+  "🎈",
+  "🌹",
+  "💄",
+  "🎀",
+  "⚽",
+  "🎾",
+  "🏁",
+  "😡",
+  "👿",
+  "🐻",
+  "🐶",
+  "🐬",
+  "🐟",
+  "🍀",
+  "👀",
+  "🚗",
+  "🍎",
+  "💝",
+  "💙",
+  "👌",
+  "❤",
+  "😍",
+  "😉",
+  "😓",
+  "😳",
+  "💪",
+  "💩",
+  "🍸",
+  "🔑",
+  "💖",
+  "🌟",
+  "🎉",
+  "🌺",
+  "🎶",
+  "👠",
+  "🏈",
+  "⚾",
+  "🏆",
+  "👽",
+  "💀",
+  "🐵",
+  "🐮",
+  "🐩",
+  "🐎",
+  "💣",
+  "👃",
+  "👂",
+  "🍓",
+  "💘",
+  "💜",
+  "👊",
+  "💋",
+  "😘",
+  "😜",
+  "😵",
+  "🙏",
+  "👋",
+  "🚽",
+  "💃",
+  "💎",
+  "🚀",
+  "🌙",
+  "🎁",
+  "⛄",
+  "🌊",
+  "⛵",
+  "🏀",
+  "🎱",
+  "💰",
+  "👶",
+  "👸",
+  "🐰",
+  "🐷",
+  "🐍",
+  "🐫",
+  "🔫",
+  "👄",
+  "🚲",
+  "🍉",
+  "💛",
+  "💚",
+];
+
+export const initializeTiles = (emojiCount, tileCount) => {
+  const emojis = generateEmojis(emojiCount, tileCount);
+  return generateTiles(emojis, tileCount);
+};
+
+const generateTiles = (content, count) => {
+  const generatedTiles = [];
+  for (let i = 0; i < count * count; i++) {
+    const tile = {
+      id: i,
+      matched: false,
+      flipped: false,
+      content: content[i],
+    };
+
+    generatedTiles.push(tile);
+  }
+  return generatedTiles;
+};
+
+const generateEmojis = (count) => {
+  let emojis = [];
+  let emojisGenerated = 0;
+
+  while (emojisGenerated < count) {
+    let randomEmoji = EMOJI_LIST[Math.floor(Math.random() * EMOJI_LIST.length)];
+    if (emojis.includes(randomEmoji)) continue;
+
+    emojis.push(randomEmoji);
+    emojis.push(randomEmoji);
+    emojisGenerated += 1;
+  }
+
+  return shuffleArray(emojis);
+};
+
+const shuffleArray = (list) => {
+  for (var index = list.length - 1; index > 0; index--) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+
+    const tempValue = list[index];
+    list[index] = list[randomIndex];
+    list[randomIndex] = tempValue;
+  }
+  return list;
+};
+
+export const mapRange = (value, low1, high1, low2, high2) => {
+  return low2 + ((high2 - low2) * (value - low1)) / (high1 - low1);
+};
+
+export const round = (value, decimalPlaces = 2) => {
+  return parseFloat(
+    Number(
+      Math.round(parseFloat(value + "e" + decimalPlaces)) + "e-" + decimalPlaces
+    ).toFixed(decimalPlaces)
+  );
+};
