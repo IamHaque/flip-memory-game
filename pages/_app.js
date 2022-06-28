3a04d6cd08531fd3053b99e8ad261ff6f157f91f
Subhan Ul Haque <iamhaque@gmail.com>
2022-06-28 15:39:59 +0530

    Revert "Added API"
    This reverts commit 7ee8afba965effc9a4cf2175a958b4f17fab7c79.

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,7 @@
-import Head from "next/head";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Tile Match</title>
-        <meta
-          name="description"
-          content="Flip memory game to match similar tiles"
-        />
-      </Head>
-
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,138 +1,373 @@
 import { useEffect, useState } from "react";
 
-import Link from "next/link";
+import { mapRange, initializeTiles } from "../shared/utils";
 
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
+import Tile from "../components/tile";
+import GridSelectButton from "../components/grid-selection-button";
 
 export default function Home() {
-  const [username, setUsername] = useState("");
-  const [userExists, setUserExists] = useState(false);
-  const [response, setResponse] = useState(undefined);
+  const GRID_SIZES = [2, 4, 6, 8];
 
+  const [gameState, setGameState] = useState({
+    tiles: [],
+    newGame: true,
+    gameWon: false,
+    gameOver: false,
+    gridSize: undefined,
+    highScores: undefined,
+    uniqueTiles: undefined,
+    totalAttempts: undefined,
+    remainingAttempts: undefined,
+  });
+
+  const [userDeviceData, setUserDeviceData] = useState({
+    langStatus: null,
+    userVendor: null,
+    userAgentStatus: null,
+    userAgentDataStatus: null,
+  });
+
+  const [timer, setTimer] = useState(0);
+  const [isTimerActive, setIsTimerActive] = useState(false);
+
+  const [prevFlippedTileIndex, setPrevFlippedTileIndex] = useState(undefined);
+
+  // Initialize game data on page load
   useEffect(() => {
-    let localUsername = localStorage.getItem("tileMatchUsername");
-    localUsername = JSON.parse(localUsername);
+    restartGame();
 
-    if (localUsername) {
-      setUserExists(true);
-      setUsername(localUsername);
-    }
+    setUserDeviceData({
+      langStatus: navigator.language,
+      userVendor: navigator.vendor,
+      userAgentStatus: navigator.userAgent,
+      userAgentDataStatus: navigator.userAgentData,
+    });
   }, []);
 
-  const registerUser = async () => {
-    const requestOptions = {
-      method: "POST",
-      body: JSON.stringify({ username }),
-      headers: { "Content-Type": "application/json" },
+  // Update game state
+  useEffect(() => {
+    if (gameState.gameOver) {
+      // Save high score to local storage
+      const currentScore = Math.floor(
+        (gameState.remainingAttempts / gameState.totalAttempts) * 100
+      );
+      if (currentScore > gameState.highScores[gameState.gridSize]) {
+        const updatedHighScores = { ...gameState.highScores };
+        updatedHighScores[gameState.gridSize] = currentScore;
+
+        localStorage.setItem(
+          "tileMatchHighScores",
+          JSON.stringify(updatedHighScores)
+        );
+        setGameState({ ...gameState, highScores: updatedHighScores });
+      }
+
+      resetTimer();
+      return;
+    }
+
+    if (gameState.tiles.length <= 0) return;
+
+    if (gameState.remainingAttempts <= 0) {
+      setGameState({ ...gameState, gameOver: true });
+      return;
+    }
+
+    let hasWon = true;
+    for (let tile of gameState.tiles) {
+      if (tile.matched) continue;
+      hasWon = false;
+      break;
+    }
+    if (hasWon) setGameState({ ...gameState, gameOver: true, gameWon: true });
+  }, [gameState]);
+
+  // Decrement remaining attempts every 5 seconds of inactivity
+  useEffect(() => {
+    if (timer % 5 === 0 && timer > 0) {
+      setGameState({
+        ...gameState,
+        remainingAttempts: gameState.remainingAttempts - 1,
+      });
+    }
+  }, [timer]);
+
+  // Update timer every second
+  useEffect(() => {
+    let interval = null;
+    if (isTimerActive) {
+      interval = setInterval(() => {
+        setTimer((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isTimerActive && timer !== 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [timer, isTimerActive]);
+
+  // Initializes the game state
+  const initializeGame = (gridSize = 2, isNewGame = true) => {
+    const uniqueTiles = (gridSize * gridSize) / 2;
+    const generatedTiles = initializeTiles(uniqueTiles, gridSize);
+
+    let localHighScores = JSON.parse(
+      localStorage.getItem("tileMatchHighScores")
+    );
+    if (!localHighScores) {
+      localHighScores = {
+        2: 0,
+        4: 0,
+        6: 0,
+        8: 0,
+      };
+    }
+
+    const newGameState = {
+      gameWon: false,
+      gameOver: false,
+      gridSize: gridSize,
+      newGame: isNewGame,
+      tiles: generatedTiles,
+      uniqueTiles: uniqueTiles,
+      highScores: localHighScores,
+      totalAttempts: Math.floor(uniqueTiles * 2.25),
+      remainingAttempts: Math.floor(uniqueTiles * 2.25),
     };
 
-    const response = await fetch("/api/addNewUser", requestOptions);
-    const data = await response.json();
+    setGameState(newGameState);
+    setPrevFlippedTileIndex(-1);
+  };
 
-    setResponse(data);
+  // Reset game state
+  const restartGame = () => {
+    initializeGame();
+  };
 
-    if (data.status && data.status === "success") {
-      localStorage.setItem("tileMatchUsername", JSON.stringify(username));
-      setUserExists(true);
+  const toggleTimer = () => {
+    setIsTimerActive(!isTimerActive);
+  };
+
+  const restartTimer = () => {
+    setTimer(0);
+  };
+
+  const resetTimer = () => {
+    setTimer(0);
+    setIsTimerActive(false);
+  };
+
+  const tileClickHandler = (tileID) => {
+    // Return if clicked on already matched tile
+    if (gameState.tiles[tileID].matched) return;
+
+    // Return if clicked on last flipped tile
+    if (prevFlippedTileIndex >= 0 && prevFlippedTileIndex === tileID) return;
+
+    let isGameOver = false;
+    let remainingAttempts = gameState.remainingAttempts;
+
+    if (prevFlippedTileIndex >= 0) {
+      setPrevFlippedTileIndex(-1);
+
+      // Checks if last flipped tile and current tile match
+      if (
+        gameState.tiles[prevFlippedTileIndex].content ===
+        gameState.tiles[tileID].content
+      ) {
+        gameState.tiles[tileID].matched = true;
+        gameState.tiles[prevFlippedTileIndex].matched = true;
+      } else {
+        // Decrement attempt if guessed incorrectly
+        remainingAttempts -= 1;
+
+        // Game is over if all attempts are used
+        if (remainingAttempts <= 0) {
+          isGameOver = true;
+        }
+
+        restartTimer();
+      }
+    } else {
+      setPrevFlippedTileIndex(tileID);
     }
+
+    // Flip tiles
+    const flippedTiles = gameState.tiles.map((tile) => {
+      return {
+        ...tile,
+        flipped: tile.matched
+          ? true
+          : tile.id === tileID ||
+            (prevFlippedTileIndex >= 0 && tile.id === prevFlippedTileIndex)
+          ? true
+          : false,
+      };
+    });
+
+    setGameState({
+      ...gameState,
+      gameOver: isGameOver,
+      tiles: [...flippedTiles],
+      remainingAttempts: remainingAttempts,
+    });
   };
 
-  const usernameChangeHandler = () => {
-    setUsername("");
-    setUserExists(false);
-    setResponse(undefined);
-    localStorage.setItem("tileMatchUsername", JSON.stringify(""));
+  const gridSelectionButtonClickHandler = (selectedGridSize) => {
+    initializeGame(selectedGridSize, false);
+    toggleTimer();
   };
 
-  let MainContent;
-  if (
-    userExists ||
-    (response && response.status && response.status === "success")
-  ) {
-    MainContent = (
-      <div className={styles.greetUser}>
-        <p>
-          Welcome{" "}
-          <span className={styles.username}>
-            {username === "" ? "Anon" : username}
-          </span>
-        </p>
-        <p className={styles.small}>
-          be prepared to get your memory retention skills tested...
-        </p>
-      </div>
-    );
-  } else {
-    MainContent = (
-      <div className={styles.inputContainer}>
-        <div className={styles.inputWrapper}>
-          <label for="username">Enter username</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div
-          className={styles.button}
-          onClick={() => {
-            registerUser();
-          }}
-        >
-          Check Availability
-        </div>
-        <small className={styles.error}>
-          {response && response.status && response.status === "failed"
-            ? response.message
-            : ""}
-        </small>
+  // Show game-over overlay if game is over
+  let GameEndOverlay = <></>;
+  if (gameState.gameWon || gameState.gameOver) {
+    GameEndOverlay = (
+      <div className="gameOverScreen">
+        <header>
+          <p>{gameState.gameWon ? "You Won!!" : "You lose!!"}</p>
+        </header>
+
+        <main>
+          <p className="score">
+            <span>Score</span>
+            <span>
+              {Math.floor(
+                (gameState.remainingAttempts / gameState.totalAttempts) * 100
+              )}
+              %
+            </span>
+          </p>
+          <p className="score highScore">
+            <span>High Score</span>
+            <span>{gameState.highScores[gameState.gridSize]}%</span>
+          </p>
+        </main>
+
+        <footer>
+          <div
+            className="restartGameButton"
+            onClick={() => {
+              restartGame();
+            }}
+          >
+            <span>Restart</span>
+          </div>
+        </footer>
       </div>
     );
   }
 
-  let ChangeUsernameButton;
-  if (userExists) {
-    ChangeUsernameButton = (
-      <div
-        className={`${styles.changeUsername}`}
-        onClick={() => {
-          usernameChangeHandler();
-        }}
-      >
-        Change Username
+  // Set game header based on new game or running game
+  let GameHeaderContent;
+  if (gameState.newGame) {
+    GameHeaderContent = (
+      <p className="description">
+        Select a grid layout from the below options.
+        <br />
+        Each option scales exponentially in difficulty.
+      </p>
+    );
+  } else {
+    GameHeaderContent = (
+      <>
+        <p className="description">
+          You have{" "}
+          <span className="strong">{gameState.totalAttempts} attempts</span> to
+          match all the unmatched tiles. An attempt will be removed every{" "}
+          <span className="strong">5 seconds</span>.
+        </p>
+
+        <div className="progressContainer">
+          <div className="progressContainerTop">
+            <p className="progressTitle">Attempts remaining</p>
+            <p className="progressStatus">
+              {gameState.remainingAttempts} / {gameState.totalAttempts}
+            </p>
+          </div>
+
+          <div
+            className="progressContainerBottom"
+            style={{
+              "--progressBarRight": `${
+                (gameState.remainingAttempts / gameState.totalAttempts) * 100
+              }%`,
+            }}
+          ></div>
+        </div>
+      </>
+    );
+  }
+
+  // Set game main content based on new game or running game
+  let GameMainContent;
+  if (gameState.newGame) {
+    GameMainContent = (
+      <div className="gridSelection main">
+        {GRID_SIZES.map((item, index) => (
+          <GridSelectButton
+            key={index}
+            gridSize={item}
+            hue={mapRange(index, 0, 3, 260, 360) * -1}
+            clickHandler={gridSelectionButtonClickHandler}
+          />
+        ))}
       </div>
+    );
+  } else {
+    GameMainContent = (
+      <>
+        <div
+          className="main"
+          style={{
+            gap: `${mapRange(gameState.gridSize, 2, 8, 1.5, 0.3)}em`,
+            gridTemplateRows: `repeat(${gameState.gridSize}, 1fr)`,
+            gridTemplateColumns: `repeat(${gameState.gridSize}, 1fr)`,
+          }}
+        >
+          {gameState.tiles.map((tile) => (
+            <Tile
+              id={tile.id}
+              key={tile.id}
+              content={tile.content}
+              flipped={tile.flipped}
+              clickHandler={tileClickHandler}
+              fontSize={`${mapRange(gameState.gridSize, 2, 8, 3, 1.5)}rem`}
+            />
+          ))}
+        </div>
+        <div
+          className="restartMainButton"
+          onClick={() => {
+            restartGame();
+          }}
+        >
+          Restart Game
+        </div>
+      </>
     );
   }
 
   return (
-    <div className="container">
-      <header className={`${styles.header} center`}>
-        <p className="title">Tile Match</p>
-      </header>
+    <>
+      <Head>
+        <title>Tile Match</title>
+        <meta
+          name="description"
+          content="Flip memory game to match similar tiles"
+        />
+      </Head>
 
-      <main className={`${styles.main} center`}>
-        {MainContent}
+      <div className="container">
+        {GameEndOverlay}
 
-        <div className={styles.bottomButtons}>
-          <div
-            className={`${styles.startGameButton} ${
-              userExists ? "" : "disabled"
-            }`}
-          >
-            <Link
-              href={{
-                pathname: "/game",
-                query: { username: username },
-              }}
-            >
-              Start Game
-            </Link>
-          </div>
-
-          {ChangeUsernameButton}
+        <div className="header">
+          <p className="title">Tile Match</p>
+          {GameHeaderContent}
         </div>
-      </main>
-    </div>
+
+        {GameMainContent}
+      </div>
+    </>
   );
 }
