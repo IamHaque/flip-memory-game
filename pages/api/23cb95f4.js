2466d6e5482dcb477f5acb808c5a55fac3e18909
Subhan Ul Haque <iamhaque@gmail.com>
2022-07-05 13:21:09 +0530

    updated api endpoint

--- pages/api/23cb95f4.js
+++ pages/api/23cb95f4.js
@@ -7,7 +7,13 @@ export default async function handler(req, res) {
   }
 
   try {
-    const allUsers = await prisma.users.findMany();
+    const allUsers = await prisma.users.findMany({
+      orderBy: [
+        {
+          updatedAt: "desc",
+        },
+      ],
+    });
     return res.status(200).json({ data: allUsers });
   } catch (e) {
     return res.status(404).json({ data: "Route not defined" });
